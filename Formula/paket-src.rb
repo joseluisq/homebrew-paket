15c35fe0a80eb19170f6885a685ff0baa0e51d10
Jose Quintana <joseluisquintana20@gmail.com>
2022-06-27 22:02:48 +0200

    fix: build from source formula

--- Formula/paket-src.rb
+++ Formula/paket-src.rb
@@ -1,10 +1,9 @@
 # typed: false
 # frozen_string_literal: true
 
-class Paket < Formula
+class PaketSrc < Formula
   desc 'A simple and fast package manager for the Fish shell written in Rust'
   homepage 'https://github.com/joseluisq/paket/'
-  license any_of: ['MIT', 'Apache-2.0']
   url 'https://github.com/joseluisq/paket/archive/v0.1.1.tar.gz'
   sha256 'c33670eb9f39ce23345d8fe780bdd50d7f34f95f0c523f14f98ca8af416e4262'
   license any_of: ['MIT', 'Apache-2.0']
